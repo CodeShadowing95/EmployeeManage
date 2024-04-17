e1f04e0747a00b195128d5806dc15459ef9c6c05
VastoLorde <ksenbonsakura@gmail.com>
2024-04-17 02:25:10 +0200

    First commit

--- empmanager-front/src/app/app.component.ts
+++ empmanager-front/src/app/app.component.ts
@@ -13,52 +13,43 @@ import { basicTailwindColors } from './constants';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  // Datas
+  // *************** Datas ***************
   public employees: Employee[] = [];
-  dataSubject: Employee[] = [];
+  private dataSubject: Employee[] = [];
+  dataLength: number = 0;
   getEmployee!: Employee;
-
-  showModal: boolean = false;
-  // selectedId: number = 0;
   optionDesc: string = '';
   companies: string[] = [];
-  dropdownId: string = 'company';
+  tempCompanies: string[] = [];
 
   selectedCompanies: string[] = [];
-  selectedType: string = 'All';
 
   jobTypes = [
-    {
-      name: "All",
-      color: "gray"
-    },
-    {
-      name: "Full Time",
-      color: "green"
-    },
-    {
-      name: "Part Time",
-      color: "blue"
-    },
-    {
-      name: "Contract",
-      color: "orange"
-    },
-    {
-      name: "Freelance",
-      color: "purple"
-    },
-    {
-      name: "Internship",
-      color: "red"
-    },
-    {
-      name: "Apprenticeship",
-      color: "purple"
-    }
+    { name: "Full Time", color: "green" },
+    { name: "Part Time", color: "blue" },
+    { name: "Contract", color: "orange" },
+    { name: "Freelance", color: "purple" },
+    { name: "Internship", color: "red" },
+    { name: "Apprenticeship", color: "purple" },
+    { name: "Remote", color: "cyan" },
+    { name: "Temporary", color: "yellow" },
+    { name: "Agency", color: "pink" },
+    { name: "Consultant", color: "teal" },
+    { name: "Visa Sponsor", color: "indigo" },
+    { name: "Intern", color: "gray" }
   ];
+  types = this.jobTypes;
 
-  // Front configs
+  // Pagination
+  currentPage: number = 1;
+  numberOfPages: number = 1;
+
+
+  // *************** Front configs ***************
+  dropdownId: string = 'company';
+  selectedType: string = 'All';
+  showModal: boolean = false;
+  // selectedId: number = 0;
   sidebarCollapsed: boolean = false;
   enableConfig: boolean = true;
   isDarkMode: boolean = false;
@@ -66,6 +57,11 @@ export class AppComponent implements OnInit {
   tabType: string = 'line';
   colors: Color[] = basicTailwindColors;
   currentColor: string = '';
+  openDropdownFilter: boolean = false;
+  currentFilter: string = 'company';
+  openDropdownSort: boolean = false;
+  sortTerm: { name: string, value: string } = { name: '', value: '' };
+  sortOrder: { name: string, value: string }  = { name: '', value: '' };
 
   /**
    * This constructor injects an instance of EmployeeService.
@@ -82,11 +78,19 @@ export class AppComponent implements OnInit {
 
   // CRUD Operations
   public getEmployees(): void {
+    const startIndex = (this.currentPage - 1) * 12;
+    const endIndex = startIndex + 12;
+
     this.employeeService.getEmployees().subscribe(
       (response: Employee[]) => {
-        this.employees = response;
+        this.employees = response.slice(startIndex, endIndex);
+        this.numberOfPages = Math.ceil(response.length / 12);
+
+        // this.dataSubject = response;
         this.dataSubject = response;
-        this.companies = Array.from(new Set(this.employees.map(employee => employee.companyName)));
+        this.dataLength = response.length;
+        this.companies = Array.from(new Set(response.map(employee => employee.companyName)));
+        this.tempCompanies = this.companies;
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
@@ -139,7 +143,7 @@ export class AppComponent implements OnInit {
   public filterEmployees(event: Event): void {
     // console.log((event.target as HTMLInputElement).value);
     const filterValue = (event.target as HTMLInputElement).value;
-    this.employees = this.dataSubject.filter(employee => employee.name.toLowerCase().includes(filterValue.toLowerCase()));
+    this.employees = this.dataSubject.filter(employee => employee.name.toLowerCase().includes(filterValue.toLowerCase()) || employee.companyName.toLowerCase().includes(filterValue.toLowerCase()));
     
 
     if(!filterValue) {
@@ -164,6 +168,11 @@ export class AppComponent implements OnInit {
     }
   }
 
+  public searchCompanies(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.companies = this.tempCompanies.filter(company => company.toLowerCase().includes(filterValue.toLowerCase()));
+  }
+
   filterByType(type: string) {
     this.selectedType = type;
 
@@ -175,9 +184,45 @@ export class AppComponent implements OnInit {
     }
   }
 
-  public getJobs(): string[] {
+  searchWorkType(event: Event) {
+    const job = (event.target as HTMLInputElement).value;
+    this.jobTypes = this.types.filter(type => type.name.toLowerCase().includes(job.toLowerCase()));
+  }
+
+  getJobs(): string[] {
     const uniqueJobs = new Set(this.employees.map(employee => employee.jobTitle));
     return Array.from(uniqueJobs);
+  }
+
+  previousPage() {
+    if(this.currentPage > 1) {
+      this.currentPage--;
+      console.log(this.currentPage);
+      
+      this.getEmployees();
+    }
+  }
+
+  nextPage() {
+    if(this.currentPage < this.numberOfPages) {
+      this.currentPage++;
+      console.log(this.currentPage);
+      this.getEmployees();
+    }
+  }
+
+  goToPage(page: number) {
+    this.currentPage = page;
+    this.getEmployees();
+  }
+
+  goToInputPage(pageInput: NgForm) {
+    if(pageInput.value?.page < 1 || pageInput.value?.page > this.numberOfPages) {
+      this.currentPage = 1;
+    } else {
+      this.currentPage = pageInput.value?.page;
+    }
+    this.getEmployees();
   }
 
 
@@ -192,6 +237,7 @@ export class AppComponent implements OnInit {
       .toUpperCase();
   }
 
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++ Front end only +++++++++++++++++++++++++++++++++++++++++++++++++++++++
   // toggleSelected(id: number, employee: Employee) {
   //   this.updateEmployee = employee;
   //   this.selectedId = id;
@@ -225,8 +271,6 @@ export class AppComponent implements OnInit {
       this.dropdownId = id;
     }
   }
-
-  // Front end only
   collapseSidebar() {
     this.sidebarCollapsed = !this.sidebarCollapsed;
   }
@@ -254,4 +298,84 @@ export class AppComponent implements OnInit {
   changeColor(color?: string): void {
     this.currentColor = color ? color : '';    
   }
+
+  toggleFilterDropdown() {
+    this.openDropdownSort = false;
+    this.openDropdownFilter = !this.openDropdownFilter;
+  }
+
+  changeFilter(filterName: string): void {
+    this.currentFilter = filterName;
+  }
+
+  toggleSortDropdown() {
+    this.openDropdownFilter = false;
+    this.openDropdownSort = !this.openDropdownSort;
+  }
+
+  changeSortTerm(term: string): void {
+    this.sortTerm.name = term;
+    if(term === 'name') {
+      this.sortTerm.value = 'Employee Name';
+    } else if(term === 'companyName') {
+      this.sortTerm.value = 'Company Name';
+    } else if(term === 'jobTitle') {
+      this.sortTerm.value = 'Job Title';
+    }
+  }
+
+  changeSortOrder(order: string): void {
+    this.sortOrder.name = order;
+    if(order === 'asc') {
+      this.sortOrder.value = 'A-Z';
+    } else if(order === 'desc') {
+      this.sortOrder.value = 'Z-A';
+    }
+  }
+
+  applyFilters(): void {
+    this.openDropdownSort = false;
+    
+    if(this.sortOrder.name === 'asc') {
+      if(this.sortTerm.name === 'name') {
+        this.employees.sort((a, b) => a.name?.localeCompare(b.name ?? '') ?? 0);
+        return;
+      }
+      if(this.sortTerm.name === 'companyName') {
+        this.employees.sort((a, b) => a.companyName?.localeCompare(b.companyName ?? '') ?? 0);
+        return;
+      }
+      if(this.sortTerm.name === 'jobTitle') {
+        this.employees.sort((a, b) => a.jobTitle?.localeCompare(b.jobTitle ?? '') ?? 0);
+        return;
+      }
+    } else {
+      if(this.sortTerm.name === 'name') {
+        this.employees.sort((a, b) => b.name?.localeCompare(a.name ?? '') ?? 0);
+        return;
+      }
+      if(this.sortTerm.name === 'companyName') {
+        this.employees.sort((a, b) => b.companyName?.localeCompare(a.companyName ?? '') ?? 0);
+        return;
+      }
+      if(this.sortTerm.name === 'jobTitle') {
+        this.employees.sort((a, b) => b.jobTitle?.localeCompare(a.jobTitle ?? '') ?? 0);
+        return;
+      }
+    }
+  }
+
+  closeDropdowns() {
+    this.openDropdownFilter = false;
+    this.openDropdownSort = false;
+  }
+
+  resetFilters(): void {
+    this.getEmployees();
+    this.sortTerm = { name: '', value: '' };
+    this.sortOrder = { name: '', value: '' };
+    this.openDropdownSort = false;
+  }
+
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++ Front end only +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 }
